415ee64255d27badf3697068ec0317105728b334
emilian0p <102601927+emilian0p@users.noreply.github.com>
2022-07-26 20:53:46 +0200

    static header no style

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,21 @@
 <template>
-    <div>
-        <h1>Header</h1>
-    </div>
+    <header>
+        <nav class="navbar">            
+            <div class="navbar-container">
+                <img src="../assets/logo.png" alt="">
+                <ul class="navbar-menu">
+                    <li class="navbar-item" ><a href="#" id="home-page">Home</a></li>
+                    <li class="navbar-item" ><a href="#" id="apple-page">Apple</a></li>
+                    <li class="navbar-item" ><a href="#" id="microsoft-page">Microsoft</a></li>
+                    <li class="navbar-item" ><a href="#" id="android-page">Android</a></li>
+                    <li class="navbar-item" ><a href="#" id="forums-page">Forums</a></li>
+                    <li class="navbar-item" ><a href="#" id="contact-page">Contact us</a></li>
+                    <li class="navbar-btn" ><a href="#" id="join-us">JOIN US</a></li>
+                    <li class="navbar-item"><a href="#"><!-- lente --></a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
 </template>
 
 <script>
@@ -11,5 +25,9 @@ export default {
 </script>
 
 <style lang="scss">
-
+.navbar {
+    background-color: #27aabe;
+    height: 80px;
+    display: flex;
+}
 </style>
