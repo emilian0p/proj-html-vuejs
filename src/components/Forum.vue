65beccc5fafd588099cc0ec380a6494a8b3b1e89
emilian0p <102601927+emilian0p@users.noreply.github.com>
2022-07-28 17:43:59 +0200

    Forum section (not ended)

--- src/components/Forum.vue
+++ src/components/Forum.vue
@@ -3,19 +3,19 @@
     <div class="forum">
         <h3>PHASELLUS EGET METUS</h3>
         <p class="font">Forum Sections</p>
-        <div class="space"></div> 
-        <div class="container up">
-            <div class="forum-items-container"><p>News & Community</p></div>
-            <div class="forum-items-container"><p>Apple Forum</p></div>
-            <div class="forum-items-container"><p>Microsoft Forum</p></div>
-            <div class="forum-items-container"><p>Android Forum</p></div>
-        </div>
-        <div class="container down">
-            <div class="forum-items-container"><p>General Discussion</p></div>
-            <div class="forum-items-container"><p>Apps & Software</p></div>
-            <div class="forum-items-container"><p>Gadgets and stuff</p></div>
-            <div class="forum-items-container"><p>Tutorials & Guides</p></div>
-        </div>
+        <div class="space"></div>
+            <div class="container">
+                <div class="forum-items-container"><p>News & Community</p></div>
+                <div class="forum-items-container"><p>Apple Forum</p></div>
+                <div class="forum-items-container"><p>Microsoft Forum</p></div>
+                <div class="forum-items-container"><p>Android Forum</p></div>
+            </div>
+            <div class="container">
+                <div class="forum-items-container"><p>General Discussion</p></div>
+                <div class="forum-items-container"><p>Apps & Software</p></div>
+                <div class="forum-items-container"><p>Gadgets and stuff</p></div>
+                <div class="forum-items-container"><p>Tutorials & Guides</p></div>
+            </div>
     </div>
 </div>
 </template>
@@ -27,7 +27,60 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-div{
+div#back{;
+    background-color: #aeaeae;
+    height: 50vh;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    text-align: center;
+    justify-content: center;   
+        h3 {
+            font-size: .8rem;
+            margin-bottom: 1rem;
+            margin-top: 12rem;
+        }
+        .font {
+            font-size: 2rem;
+            margin-bottom: 1rem;
+            font-weight: bold;
+        }
+        .space{
+            background-color: #fc8c5e;
+            height: 2px;
+            width: 4rem;
+            justify-content: center;
+            align-content: center;
+            margin-bottom: 20px;
+            margin-left: 17rem;
+        }
+        .container{
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
+            align-items: center;
+            background-color: #fff;
+            height: 10rem;
+                    .forum-items-container{ 
+                        display: flex;
+                        flex-direction: column;
+                        justify-content: center;
+                        align-items: center;
+                        width: 100%;
+                        margin-bottom: 2rem;
+                        margin-top: 2rem;
+                            p{
+                                font-size: .7rem;
+                            }
+                    }
+            }
+        }
+
+
+
+
+
+/* div{
     display: flex;
 }
 div#back{
@@ -78,5 +131,5 @@ p{
     font-size: .7rem;
     margin-bottom: 1rem;
     
-}
+} */
 </style>
